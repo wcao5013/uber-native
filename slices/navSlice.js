15368d000a68fa529ae59c6657feacdd0ab729d8
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-20 16:37:01 -0400

    message

---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action ) => {
+            state.origin = action.payload
+        },
+        setDestination : ( state, action ) => {
+            state.destination = action.payload
+        },
+        setTraveTimeInformation: (state, action ) => {
+            state.traveTimeInformation = action.payload
+        }
+    }
+    
+})
+
+export const { setOrigin, setDestination, setTraveTimeInformation } = navSlice.actions
+
+//Selectors
+export const selectOrigin = ( state ) => state.nav.origin
+export const selectDestination = ( state ) => state.nav.destination
+export const selectTimeTravelInformation = ( state ) => state.nav.traveTimeInformation
+
+export default navSlice.reducer
