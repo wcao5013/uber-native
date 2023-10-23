6caeb2e2ce72aa7ba2c2a17450a6b52ebe2e3dcc
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-23 14:58:13 -0400

    message

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,33 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { Text, StyleSheet, View, SafeAreaView, TouchableOpacity } from 'react-native'
-import { Icon } from 'react-native-elements'
+import React, { useState } from 'react'
+import { Text, StyleSheet, View, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
+import { Icon, Image } from 'react-native-elements'
 import tw from 'tailwind-react-native-classnames'
+
+const data = [
+  {
+    id: 'Uber-X-123',
+    title: 'Uber X',
+    multiplier: 1,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_538/v1568070387/assets/b5/0a5191-836e-42bf-ad5d-6cb3100ec425/original/UberX.png'
+  },
+  {
+    id: 'Uber-XL-456',
+    title: 'Uber XL',
+    multiplier: 1.2,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_537/v1568134115/assets/6d/354919-18b0-45d0-a151-501ab4c4b114/original/XL.png'
+  },
+  {
+    id: 'Uber-LUX-789',
+    title: 'Uber LUX',
+    multiplier: 1.75,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_538/v1568070443/assets/82/6bf372-6016-492d-b20d-d81878a14752/original/Black.png'
+  },
+]
 
 const RideOptionsCard = () => {
     const navigation = useNavigation()
+    const [selected, setSelected] = useState(null)
 
     return (
       <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -17,6 +39,32 @@ const RideOptionsCard = () => {
           </TouchableOpacity>
           <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
         </View>
+        <FlatList 
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item: { id, title, multiplier, image }, item }) => (
+            <TouchableOpacity 
+              onPress={() => setSelected(item)}
+              style={tw`flex-row justify-between items-center px-10 ${
+                  id === selected?.id && 'bg-gray-200'
+              }`}>
+              
+              <Image
+                style={{
+                  width: 100,
+                  height: 100,
+                  resizeMode: 'contain',
+                }}
+                source={{ uri: image }}
+              />
+              <View style={tw`-ml-6`}>
+                <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                <Text>Travel time...</Text>
+              </View>
+              <Text style={tw`text-xl`}>$100</Text>
+            </TouchableOpacity>
+          )}
+        />
       </SafeAreaView>
     )
 }
