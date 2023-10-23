4fac5cecd67f097d30c3fd4819250a0e2c238a7f
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-23 14:08:23 -0400

    message

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { Text, StyleSheet, View, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
@@ -8,6 +8,7 @@ import { useDispatch } from 'react-redux'
 import { setDestination } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 import NavFavorites from './NavFavorites'
+import { Icon } from 'react-native-elements'
 
 
 const NavigateCard = ( ) => {
@@ -54,6 +55,24 @@ const NavigateCard = ( ) => {
                         />
                     </View>
                     <NavFavorites />
+                </View>
+                
+                <View style={tw`flex-row bg-white  justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                    <TouchableOpacity 
+                        onPress={() => navigation.navigate('RideOptionsCard')}
+                        style={tw`flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}>
+                        
+                        <Icon name='car' type='font-awesome' color='white' size={16} />
+                        <Text style={tw`text-white text-center`}> Rides</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        // This would be changed to Eat Options card
+                        // onPress={() => navigation.navigate('RideOptionsCard')}
+                        style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+                        
+                        <Icon name='fast-food-outline' type='ionicon' color='black' size={16} />
+                        <Text style={tw`text-center`}>Eats</Text>
+                    </TouchableOpacity>
                 </View>
             </SafeAreaView>
         </TouchableWithoutFeedback>
