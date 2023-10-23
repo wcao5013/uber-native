9e58738399786b014f68687c1fa2c1e45bf889ad
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-23 19:27:30 -0400

    message

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
 import Map from '../components/Map'
 import MapView from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator()
+    const navigation = useNavigation()
 
     return (
       <View>
+        <TouchableOpacity
+            onPress={() => navigation.navigate('HomeScreen')}
+            style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+        >
+            <Icon name='menu' />
+        </TouchableOpacity>
         <View style={tw`h-1/2`}>
             <Map />
         </View>
